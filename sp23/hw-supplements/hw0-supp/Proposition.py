ed976759616d354ccd56fd484a64de5a08861aa9
Sarthak Mohanty <smohanty61@gatech.edu>
2023-01-14 18:28:37 -0500

    uploaded hw0-supp

--- sp23/hw-supplements/hw0-supp/Proposition.py
+++ sp23/hw-supplements/hw0-supp/Proposition.py
@@ -1,13 +1,13 @@
-# CS 2051 Spring 2023 - Programming Assignment 1 : Python Primer
-# instructor: Gerandy Brito
-# author: Sarthak Mohanty (replace with your name)
+# CS 2051 Spring 2023 - HW0 Supplement : Python Primer
+# Instructor: Gerandy Brito
+# Created by Sarthak Mohanty
 
 # This assignment is designed to help you get familiar with Python and submitting assignments on Gradescope.
 # This assignment is ungraded.
 
 # First, create a class called Proposition.
 # It takes in a string as an argument.
-# Assume the string is a valid propositional formula that uses the variables p - z and the operators and, or, not.
+# Assume the string is a valid propositional formula that uses the variables p - z and the operators 'and', 'or', and 'not'.
 # Examples of such strings are:
     # "p and (q or r)"
     # "p or q"
@@ -20,10 +20,11 @@
 # These classes should take in two Proposition objects as arguments.
 # WLOG, assume the first Proposition object is the antecedent (p) and the second is the consequent (q).
 # self.func should be set equivalent to the string "p implies q" or "p iff q" respectively.
-# However, since the Propsition class does not support the implies and iff operators,
-# you will need to represent these string using the operators and, or, and not using logical equivalencies
-#  Make sure to use parentheses appropriately.
+# However, since the Propsition class does not support the implies and iff operators, you will need to represent these strings using the operators 'and', 'or', and 'not'
+# Make sure to use parentheses appropriately.
 
 # Finally, create a method called extract_variables.
 # This method should take in a Proposition object and return a Set of all the variables in the proposition.
 # For example, if the proposition is "p and (q or r)", the method should return the set {"p", "q", "r"}.
+
+# To submit, upload this file (proposition.py) to Gradescope.
