be51f056b84e777e39635db6c67ff8f85fc4f829
Sarthak Mohanty <smohanty61@gatech.edu>
2023-02-20 20:59:24 -0500

    updated scheduler.py docstrings

--- sp23/hw-supplements/hw5-supp/scheduler.py
+++ sp23/hw-supplements/hw5-supp/scheduler.py
@@ -6,34 +6,36 @@
 # collaborators - N/A
 
 
-def topological_sort(elements, poset):
+def topological_sort(elements: set, poset: set):
     """Returns a topological sort of the given poset using a modified version of Kahn's algorithm.
     
     Parameters:
+        elements: The domain of the relation.
         poset: A set of tuples (x, y) representing a binary relation.
         
     Returns:
         A list of elements in topological order.
         
     Example:
-    >>> topological_sort({(1, 2), (2, 4), (3, 4), (4, 5), (5, 6)})
+    >>> topological_sort({1, 2, 3, 4, 5, 6}, {(1, 2), (2, 4), (3, 4), (4, 5), (5, 6)})
     [1, 3, 2, 4, 5, 6]
     """
     # YOUR CODE HERE
     return NotImplementedError
 
-def generate_schedule(elements, poset, num_processors):
+def generate_schedule(element: set, poset: set, num_processors: int):
     """Returns an optimal schedule for the given poset using the given number of processors.
     
     Parameters:
-        poset: A list of tuples (x, y) representing a binary relation.
+        elements: The domain of the relation.
+        poset: A set of tuples (x, y) representing a binary relation.
         num_processors: The number of processors available
         
     Returns:
         A list of lists of elements representing the schedule.
 
     Example:
-    >>> optimal_scheduling([(1, 2), (2, 4), (3, 4), (4, 5), (5, 6)], 2)
+    >>> optimal_scheduling({1, 2, 3, 4, 5, 6}, {(1, 2), (2, 4), (3, 4), (4, 5), (5, 6)}, 2)
     [[1, 3], [2], [4], [5], [6]]
     """
     # YOUR CODE HERE
